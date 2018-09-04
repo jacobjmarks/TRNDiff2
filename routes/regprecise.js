616648702b33f82442f8c43c4a4e319f509353b0
Jacob Marks <jacobjmarks@outlook.com>
2018-09-04 17:45:48 +1000

    Setup RegPrecise JSON filter generation from request querystring

--- routes/regprecise.js
+++ routes/regprecise.js
@@ -12,17 +12,35 @@ router.get("/status", (req, res) => {
 });
 
 router.get("/genomes", (req, res) => {
-    regprecise.genomes(g => true, (err, genomes) => {
+    regprecise.genomes(generateFilter(req.query), (err, genomes) => {
         if (err) { console.error(err); return res.status(500).end(); }
         res.json(genomes);
     });
 });
 
 router.get("/regulogs", (req, res) => {
-    regprecise.regulogs(r => true, (err, regulogs) => {
+    regprecise.regulogs(generateFilter(req.query), (err, regulogs) => {
         if (err) { console.error(err); return res.status(500).end(); }
         res.json(regulogs);
     });
 });
+
+router.get("/regulons", (req, res) => {
+    regprecise.regulogs(generateFilter(req.query), (err, regulons) => {
+        if (err) { console.error(err); return res.status(500).end(); }
+        res.json(regulons);
+    });
+})
+
+function generateFilter(reqQuery) {
+    let keys = Object.keys(reqQuery);
+    if (!keys.length) return r => true;
+    return (obj) => {
+        for (let key of keys) {
+            if (obj[key] != reqQuery[key]) return false;
+        }
+        return true;
+    }
+}
 
 module.exports = router;
