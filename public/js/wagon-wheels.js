e741e0879289c5672d7875dffdf084f5ba2439c7
Jacob Marks <jacobjmarks@outlook.com>
2018-09-04 21:40:40 +1000

    Refined wagonwheel svg drawing and setup basic tooltip

--- public/js/wagon-wheels.js
+++ public/js/wagon-wheels.js
@@ -1,3 +1,5 @@
+let columns = 4;
+
 function viewWagonWheels(regulogId) {
     $("#select-data .table").empty();
     $("#select-data .table").hide();
@@ -8,28 +10,31 @@ function viewWagonWheels(regulogId) {
         method: "GET",
         url: `/regprecise/graph/wagonwheels/regulog/${regulogId}`,
         success: (regulonNetworks) => {
-            let graph = $("#graph");
+            let graph = d3.select("#graph");
+            let svgSize = $(".ui.container").width() / columns;
 
-            let tempDiv = $("<div>").addClass("column");
-            graph.append(tempDiv);
-            let svgWidth = tempDiv.outerWidth();
-            let svgHeight = svgWidth;
-            graph.empty();
+            let tooltip = d3.select("body").append("div")
+                .style("position", "absolute")
+                .style("z-index", 10)
+                .style("visibility", "hidden")
 
             for (let regulon of regulonNetworks) {
-                let svg = $("<svg>").width(svgWidth).height(svgHeight);
+                let svg = graph.append("svg")
+                    .attr("width", svgSize)
+                    .attr("height", svgSize)
                 
-                svg.append(
-                    $("<circle>")
-                        .attr("cx", svg.width() / 2)
-                        .attr("cy", svg.height() / 2)
-                        .attr("r", 10)
-                )
-
-                graph.append(svg);
+                let centroid = svg.append("circle")
+                    .attr("cx", svgSize / 2)
+                    .attr("cy", svgSize / 2)
+                    .attr("r", 10)
+                    .on("mouseout", () => { tooltip.style("visibility", "hidden") })
+                    .on("mousemove", () => { tooltip.style("top",(d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px") })
+                    .on("mouseover", () => {
+                        tooltip
+                            .text(regulon.regulatorName)
+                            .style("visibility", "visible")
+                    })
             }
-
-            graph.html(graph.html());
         },
         error: () => {
             alert("Error fetching graph network");
