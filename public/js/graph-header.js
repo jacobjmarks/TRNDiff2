22737722cf36d201ece10f080910cabaa17463f7
Jacob Marks <jacobjmarks@outlook.com>
2018-09-20 16:07:46 +1000

    Setup basic wagonwheel regulon sorting

--- public/js/graph-header.js
+++ public/js/graph-header.js
@@ -1,6 +1,21 @@
 $(document).ready(() => {
     $("#sort-regulons-by.dropdown").dropdown({
+        action: (text, value) => {
+            regulonNetworks.sort((a, b) => {
+                switch(value) {
+                    case "distance":
+                        return a.distance - b.distance;
+                    case "gene-count":
+                        return b.targetGenes.length - a.targetGenes.length;
+                    case "genome-name":
+                        return a.genomeName.localeCompare(b.genomeName);
+                    default:
+                        return 0;
+                }
+            })
 
+            drawWagonWheels();
+        }
     });
 
     $("#sort-genes-by.dropdown").dropdown({
