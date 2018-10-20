7f8335f60d1b2fcfe1c4cba4c683f3585b29e230
Jacob Marks <jacobjmarks@outlook.com>
2018-10-20 10:14:36 +1000

    Defined singular wagonwheel drawing function

--- public/js/wagon-wheels.js
+++ public/js/wagon-wheels.js
@@ -55,32 +55,32 @@ function svgElem(tag) {
 
 function drawWagonWheels() {
     $("#graph").show();
-    const svgDivMargin = 14;
-    let svgDivSize = ($("#graph").width() - (svgDivMargin * columns)) / columns;
-    let svgSize = svgDivSize;
+    svgDivMargin = 14;
+    svgDivSize = ($("#graph").width() - (svgDivMargin * columns)) / columns;
+    svgSize = svgDivSize;
 
-    let regulons = regulogNetwork.regulons;
+    regulons = regulogNetwork.regulons;
 
-    let uniqueGenes = regulons
-                        .map(r => r.targetGenes)
-                        .reduce((a, b) => a.concat(b), [])
-                        .reduce((a, b) => {
-                            if (!a.find(g => g.name == b.name)) {
-                                a.push(b);
-                            }
-                            return a;
-                        }, [])
+    uniqueGenes = regulons
+                    .map(r => r.targetGenes)
+                    .reduce((a, b) => a.concat(b), [])
+                    .reduce((a, b) => {
+                        if (!a.find(g => g.name == b.name)) {
+                            a.push(b);
+                        }
+                        return a;
+                    }, [])
 
-    let spokeLength = svgSize * 0.75;
-    let spokeAngle = 360 / uniqueGenes.length;
+    spokeLength = svgSize * 0.75;
+    spokeAngle = 360 / uniqueGenes.length;
 
-    let origin = {
+    origin = {
         x: svgSize / 2,
         y: svgSize / 2
     }
 
-    let geneNodeRadius = Math.min(spokeLength/2 * toRadians(spokeAngle) / 2, 10);
-    let geneNodePositions = (() => {
+    geneNodeRadius = Math.min(spokeLength/2 * toRadians(spokeAngle) / 2, 10);
+    geneNodePositions = (() => {
         let positions = {};
         uniqueGenes.sort(geneSortFunc).map(g => g.name).forEach((name, i) => {
             let angle = toRadians((270 + spokeAngle * i) % 360);
@@ -96,145 +96,7 @@ function drawWagonWheels() {
     graph.empty();
 
     for (let regulon of regulons) {
-        let svgDiv = $("<div>")
-            .addClass("ui card")
-            .addClass("wagonwheel")
-            .width(svgDivSize)
-
-        if (regulon.regulonId == regulogNetwork["selected-regulon"]) {
-            svgDiv.css("border", "1px solid blue");
-        }
-
-        let svg = $(svgElem("svg"))
-            .attr({
-                "width": svgSize,
-                "height": svgSize
-            })
-        svgDiv.append(svg);
-
-        let svgFooter = $("<div>")
-                .addClass("ui centered small header")
-                .text(regulon.genomeName)
-        svgDiv.append(svgFooter);
-
-        for (let gene of regulon.targetGenes) {
-            let to = geneNodePositions[gene.name];
-
-            let color = goTermColors[gene.term];
-
-            updateGoTermLegend(gene.term);
-
-            let spoke = $(svgElem("line"))
-                .addClass(`gene-spoke gene-${gene.name}`)
-                .attr({
-                    "x1": origin.x,
-                    "y1": origin.y,
-                    "x2": to.x,
-                    "y2": to.y,
-                    "stroke-width": 2,
-                    "stroke": color
-                })
-            svg.append(spoke);
-            
-            if (!gene.sites.length) spoke.css("stroke-dasharray", "5, 5").css("opacity", 0.5);
-
-            function highlight(color) {
-                $(`svg circle.gene-node.gene-${gene.name}`).attr("fill", color);
-                $(`svg line.gene-spoke.gene-${gene.name}`).css("stroke", color);
-            }
-
-            let node = $(svgElem("circle"))
-                .addClass(`gene-node gene-${gene.name}`)
-                .data("gene-data", gene)
-                .attr({
-                    "cx": to.x,
-                    "cy": to.y,
-                    "r": geneNodeRadius,
-                    "fill": color
-                })
-                .on("mouseover", () => {
-                    highlight("blue");
-
-                    tooltip.empty();
-                    tooltip.append($("<table>")
-                        .append($("<tr>")
-                            .append($("<td>").text("Locus Tag"))
-                            .append($("<td>").text(gene.locusTag || "n/a")))
-                        .append($("<tr>")
-                            .append($("<td>").text("Gene Name"))
-                            .append($("<td>").text(gene.name)))
-                        .append($("<tr>")
-                            .append($("<td>").text("Function"))
-                            .append($("<td>").text(gene.function || "n/a")))
-                        .append($("<tr>")
-                            .append($("<td>").text("Site/s"))
-                            .append($("<td>").text(gene.sites.map(s => s.sequence).join(", ") || "n/a")))
-                        .append(!gene.sites.length ? null : $("<tr>")
-                            .append($("<td>").text("Site Position"))
-                            .append($("<td>").text(gene.sites.map(s => s.position).join(", ") || "n/a")))
-                        .append(!gene.sites.length ? null : $("<tr>")
-                            .append($("<td>").text("Site Score"))
-                            .append($("<td>").text(gene.sites.map(s => s.score).join(", ") || "n/a")))
-                    )
-                    tooltip.css("visibility", "visible")
-                })
-                .on("mousemove", () => { tooltip.css("top",(event.pageY-10)+"px").css("left",(event.pageX+10)+"px") })
-                .on("mouseout", () => {
-                    highlight(color);
-                    tooltip.css("visibility", "hidden");
-                })
-            svg.append(node);
-        }
-
-        let centroidRadius = Math.min(svgSize * 0.05, 10);
-
-        let centroidMargin = $(svgElem("circle"))
-            .addClass("centroid-margin")
-            .attr({
-                "cx": svgSize / 2,
-                "cy": svgSize / 2,
-                "r": centroidRadius * 1.5,
-                "fill": "white"
-            })
-        svg.append(centroidMargin);
-        
-        let centroid = $(svgElem("circle"))
-            .addClass("centroid")
-            .attr({
-                "cx": svgSize / 2,
-                "cy": svgSize / 2,
-                "r": centroidRadius
-            })
-            .on("mouseover", () => {
-                tooltip.empty();
-                tooltip
-                    .append($("<table>")
-                        .append($("<tr>")
-                            .append($("<td>").text("Locus Tag"))
-                            .append($("<td>").text(regulon.regulator && regulon.regulator.locusTag || "n/a")))
-                        .append($("<tr>")
-                            .append($("<td>").text("Regulator Family"))
-                            .append($("<td>").text(regulon.regulatorFamily)))
-                        .append($("<tr>")
-                            .append($("<td>").text("Regulator Name"))
-                            .append($("<td>").text(regulon.regulatorName)))
-                        .append($("<tr>")
-                            .append($("<td>").text("Regulation Type"))
-                            .append($("<td>").text(regulon.regulationType)))
-                        .append($("<tr>")
-                            .append($("<td>").text("Function"))
-                            .append($("<td>").text(regulon.regulator && regulon.regulator.function || "n/a")))
-                        .append($("<tr>")
-                            .append($("<td>").text("Target Genes"))
-                            .append($("<td>").text(regulon.targetGenes.length)))
-                    )
-                    .css("visibility", "visible")
-            })
-            .on("mousemove", () => { tooltip.css("top",(event.pageY-10)+"px").css("left",(event.pageX+10)+"px") })
-            .on("mouseout", () => { tooltip.css("visibility", "hidden") })
-        svg.append(centroid);
-
-        graph.append(svgDiv);
+        drawWagonWheel(regulon, graph);
     }
 
     // Redraw if available width has changed after drawing
@@ -242,6 +104,154 @@ function drawWagonWheels() {
     if ($("#graph").width() < (svgDivSize + svgDivMargin) * columns) {
         drawWagonWheels();
     }
+}
+
+/**
+ * Draw and append a single wagonwheel of the provided regulon to the given HTML
+ * element.
+ * @param {JSON} regulon Regulon for which to graph
+ * @param {Element} div HTML element to append wagonwheel
+ */
+function drawWagonWheel(regulon, div) {
+    let svgDiv = $("<div>")
+        .addClass("ui card")
+        .addClass("wagonwheel")
+        .width(svgDivSize)
+
+    if (regulon.regulonId == regulogNetwork["selected-regulon"]) {
+        svgDiv.css("border", "1px solid blue");
+    }
+
+    let svg = $(svgElem("svg"))
+        .attr({
+            "width": svgSize,
+            "height": svgSize
+        })
+    svgDiv.append(svg);
+
+    let svgFooter = $("<div>")
+            .addClass("ui centered small header")
+            .text(regulon.genomeName)
+    svgDiv.append(svgFooter);
+
+    for (let gene of regulon.targetGenes) {
+        let to = geneNodePositions[gene.name];
+
+        let color = goTermColors[gene.term];
+
+        updateGoTermLegend(gene.term);
+
+        let spoke = $(svgElem("line"))
+            .addClass(`gene-spoke gene-${gene.name}`)
+            .attr({
+                "x1": origin.x,
+                "y1": origin.y,
+                "x2": to.x,
+                "y2": to.y,
+                "stroke-width": 2,
+                "stroke": color
+            })
+        svg.append(spoke);
+        
+        if (!gene.sites.length) spoke.css("stroke-dasharray", "5, 5").css("opacity", 0.5);
+
+        function highlight(color) {
+            $(`svg circle.gene-node.gene-${gene.name}`).attr("fill", color);
+            $(`svg line.gene-spoke.gene-${gene.name}`).css("stroke", color);
+        }
+
+        let node = $(svgElem("circle"))
+            .addClass(`gene-node gene-${gene.name}`)
+            .data("gene-data", gene)
+            .attr({
+                "cx": to.x,
+                "cy": to.y,
+                "r": geneNodeRadius,
+                "fill": color
+            })
+            .on("mouseover", () => {
+                highlight("blue");
+
+                tooltip.empty();
+                tooltip.append($("<table>")
+                    .append($("<tr>")
+                        .append($("<td>").text("Locus Tag"))
+                        .append($("<td>").text(gene.locusTag || "n/a")))
+                    .append($("<tr>")
+                        .append($("<td>").text("Gene Name"))
+                        .append($("<td>").text(gene.name)))
+                    .append($("<tr>")
+                        .append($("<td>").text("Function"))
+                        .append($("<td>").text(gene.function || "n/a")))
+                    .append($("<tr>")
+                        .append($("<td>").text("Site/s"))
+                        .append($("<td>").text(gene.sites.map(s => s.sequence).join(", ") || "n/a")))
+                    .append(!gene.sites.length ? null : $("<tr>")
+                        .append($("<td>").text("Site Position"))
+                        .append($("<td>").text(gene.sites.map(s => s.position).join(", ") || "n/a")))
+                    .append(!gene.sites.length ? null : $("<tr>")
+                        .append($("<td>").text("Site Score"))
+                        .append($("<td>").text(gene.sites.map(s => s.score).join(", ") || "n/a")))
+                )
+                tooltip.css("visibility", "visible")
+            })
+            .on("mousemove", () => { tooltip.css("top",(event.pageY-10)+"px").css("left",(event.pageX+10)+"px") })
+            .on("mouseout", () => {
+                highlight(color);
+                tooltip.css("visibility", "hidden");
+            })
+        svg.append(node);
+    }
+
+    let centroidRadius = Math.min(svgSize * 0.05, 10);
+
+    let centroidMargin = $(svgElem("circle"))
+        .addClass("centroid-margin")
+        .attr({
+            "cx": svgSize / 2,
+            "cy": svgSize / 2,
+            "r": centroidRadius * 1.5,
+            "fill": "white"
+        })
+    svg.append(centroidMargin);
+    
+    let centroid = $(svgElem("circle"))
+        .addClass("centroid")
+        .attr({
+            "cx": svgSize / 2,
+            "cy": svgSize / 2,
+            "r": centroidRadius
+        })
+        .on("mouseover", () => {
+            tooltip.empty();
+            tooltip
+                .append($("<table>")
+                    .append($("<tr>")
+                        .append($("<td>").text("Locus Tag"))
+                        .append($("<td>").text(regulon.regulator && regulon.regulator.locusTag || "n/a")))
+                    .append($("<tr>")
+                        .append($("<td>").text("Regulator Family"))
+                        .append($("<td>").text(regulon.regulatorFamily)))
+                    .append($("<tr>")
+                        .append($("<td>").text("Regulator Name"))
+                        .append($("<td>").text(regulon.regulatorName)))
+                    .append($("<tr>")
+                        .append($("<td>").text("Regulation Type"))
+                        .append($("<td>").text(regulon.regulationType)))
+                    .append($("<tr>")
+                        .append($("<td>").text("Function"))
+                        .append($("<td>").text(regulon.regulator && regulon.regulator.function || "n/a")))
+                    .append($("<tr>")
+                        .append($("<td>").text("Target Genes"))
+                        .append($("<td>").text(regulon.targetGenes.length)))
+                )
+                .css("visibility", "visible")
+        })
+        .on("mousemove", () => { tooltip.css("top",(event.pageY-10)+"px").css("left",(event.pageX+10)+"px") })
+        .on("mouseout", () => { tooltip.css("visibility", "hidden") })
+    svg.append(centroid);
+
+    div.append(svgDiv);
 }
 
 /**
